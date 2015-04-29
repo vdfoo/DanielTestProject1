12534ca4d9117e5f7e8ddc0c00ff79759c70eb60
vdfoo <danielvundy@gmail.com>
2015-04-29 10:42:48 +0800

    Implemented for loop

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,10 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Hello World!");
+            }
         }
     }
 }
